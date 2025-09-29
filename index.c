4302cf71f8c4c5285866951acafb5d91a500d51e
Alexander Wu <wualexander518@gmail.com>
2025-09-28 20:09:44 -0400

    update file and add basic script for reading

---
+++ index.c
@@ -0,0 +1,23 @@
+// g++ -std=gnu99 -Wall -O1 index.c -o index
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdint.h>
+
+int main() {
+    FILE* collection = fopen("collection.tsv", "r");
+    if (!collection) { perror("Failed to open collection.tsv"); exit(1); }
+
+    char* line = NULL;
+    size_t lineSize = 0;
+
+    for (int i = 0; i < 5; i++) {
+        int n = getline(&line, &lineSize, collection);
+        printf(line);
+    }
+
+    free(line);
+    return 0;
+}
